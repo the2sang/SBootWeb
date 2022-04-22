52f4041205c2a0b676d615fbe92dce276dd0021c
the2sang <the2sang@gmail.com>
2022-04-22 17:26:48 +0900

    테이블명 변경 스키마 수정

--- db/EBXMLWEB_XMLEDI_MPRO_DET.sql
+++ db/EBXMLWEB_XMLEDI_MPRO_DET.sql
@@ -1,4 +1,4 @@
-create table XMLEDI_MPRO_DET
+create table IF_MPRO_DET
 (
     EBELN        VARCHAR2(10) not null,
     LIFNR        VARCHAR2(10) not null,
@@ -33,99 +33,99 @@ create table XMLEDI_MPRO_DET
     ATWRT_002_09 NUMBER(5, 2),
     RECVST       VARCHAR2(5),
     RECVMG       VARCHAR2(250),
-    constraint XMLEDI_MPRO_DET_PK
+    constraint IF_MPRO_DET_PK
         primary key (EBELN, LIFNR, LIFNR_GR, EBELP, MATSN)
 )
 /
 
-comment on column XMLEDI_MPRO_DET.EBELN is '주문번호 (ERP 자동기록)'
+comment on column IF_MPRO_DET.EBELN is '주문번호 (ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_DET.LIFNR is '계약업체 (ERP 자동기록)'
+comment on column IF_MPRO_DET.LIFNR is '계약업체 (ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_DET.LIFNR_GR is '납품업체 (ERP 자동기록)'
+comment on column IF_MPRO_DET.LIFNR_GR is '납품업체 (ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_DET.EBELP is '품목번호 (ERP 자동기록)'
+comment on column IF_MPRO_DET.EBELP is '품목번호 (ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_DET.MATSN is '고유인식번호 (ERP 자동기록)'
+comment on column IF_MPRO_DET.MATSN is '고유인식번호 (ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_DET.PRDFT is '생산공장 (사업자번호) (업체 필수입력)'
+comment on column IF_MPRO_DET.PRDFT is '생산공장 (사업자번호) (업체 필수입력)'
 /
 
-comment on column XMLEDI_MPRO_DET.PRDSN is '제작번호 (업체 필수입력)'
+comment on column IF_MPRO_DET.PRDSN is '제작번호 (업체 필수입력)'
 /
 
-comment on column XMLEDI_MPRO_DET.PRDDT is '제작일자 (업체 필수입력)'
+comment on column IF_MPRO_DET.PRDDT is '제작일자 (업체 필수입력)'
 /
 
-comment on column XMLEDI_MPRO_DET.PRNAM is '제작담당자 이름 (업체 필수입력)'
+comment on column IF_MPRO_DET.PRNAM is '제작담당자 이름 (업체 필수입력)'
 /
 
-comment on column XMLEDI_MPRO_DET.EBELN_PO is '인도지시번호 (업체 필수입력)'
+comment on column IF_MPRO_DET.EBELN_PO is '인도지시번호 (업체 필수입력)'
 /
 
-comment on column XMLEDI_MPRO_DET.EBELN_PO is '인도지시품목번호 (업체 필수입력)'
+comment on column IF_MPRO_DET.EBELN_PO is '인도지시품목번호 (업체 필수입력)'
 /
 
-comment on column XMLEDI_MPRO_DET.ATWRT_001_01 is '변압기 1차 부싱타입 (변압기이면 필수 입력) 1-자기제, 2-폴리머, 3-에폭시'
+comment on column IF_MPRO_DET.ATWRT_001_01 is '변압기 1차 부싱타입 (변압기이면 필수 입력) 1-자기제, 2-폴리머, 3-에폭시'
 /
 
-comment on column XMLEDI_MPRO_DET.ATWRT_001_02 is '변압기 2차 부싱타입 (변압기이면 필수 입력) 1-자기제, 2-폴리머, 3-에폭시'
+comment on column IF_MPRO_DET.ATWRT_001_02 is '변압기 2차 부싱타입 (변압기이면 필수 입력) 1-자기제, 2-폴리머, 3-에폭시'
 /
 
-comment on column XMLEDI_MPRO_DET.ATWRT_001_03 is '변압기 권선종류 (변압기이면 필수 입력) 1-Cu 2-AL'
+comment on column IF_MPRO_DET.ATWRT_001_03 is '변압기 권선종류 (변압기이면 필수 입력) 1-Cu 2-AL'
 /
 
-comment on column XMLEDI_MPRO_DET.ATWRT_001_04 is '변압기 절연유타입 (변압기이면 필수 입력) 1-광유 2-실리콘유  3-식물유 4-난연유'
+comment on column IF_MPRO_DET.ATWRT_001_04 is '변압기 절연유타입 (변압기이면 필수 입력) 1-광유 2-실리콘유  3-식물유 4-난연유'
 /
 
-comment on column XMLEDI_MPRO_DET.ATWRT_001_05 is '변압기 절연지 내열온도(C) (변압기이면 필수 입력 1-90 2-105 3-120 4-130 5-155 6-180 7-180 이상)'
+comment on column IF_MPRO_DET.ATWRT_001_05 is '변압기 절연지 내열온도(C) (변압기이면 필수 입력 1-90 2-105 3-120 4-130 5-155 6-180 7-180 이상)'
 /
 
-comment on column XMLEDI_MPRO_DET.ATWRT_001_06 is '변압기 유량(L) (변압기이면 필수 입력  ) 1-정수 2~4자리(10~9999)'
+comment on column IF_MPRO_DET.ATWRT_001_06 is '변압기 유량(L) (변압기이면 필수 입력  ) 1-정수 2~4자리(10~9999)'
 /
 
-comment on column XMLEDI_MPRO_DET.ATWRT_001_07 is '변압기 무부하손(W) (변압기이면 필수 입력 ) 1-정수 2~4자리(10~9999)'
+comment on column IF_MPRO_DET.ATWRT_001_07 is '변압기 무부하손(W) (변압기이면 필수 입력 ) 1-정수 2~4자리(10~9999)'
 /
 
-comment on column XMLEDI_MPRO_DET.ATWRT_001_08 is '변압기 부하손(W) (변압기이면 필수 입력 ) 1-정수 2~4자리(10~9999)'
+comment on column IF_MPRO_DET.ATWRT_001_08 is '변압기 부하손(W) (변압기이면 필수 입력 ) 1-정수 2~4자리(10~9999)'
 /
 
-comment on column XMLEDI_MPRO_DET.ATWRT_002_01 is '개폐기 정격전류(A) (개폐기이면 필수 입력) 1-Kraft 2-Diamond Kraft 3-Diamond Kraft 4 H종 노맥스지 5-기타'
+comment on column IF_MPRO_DET.ATWRT_002_01 is '개폐기 정격전류(A) (개폐기이면 필수 입력) 1-Kraft 2-Diamond Kraft 3-Diamond Kraft 4 H종 노맥스지 5-기타'
 /
 
-comment on column XMLEDI_MPRO_DET.ATWRT_002_02 is '개폐기 스위칭매체 (개폐기이면 필수 입력) 1-400 2-600 3-630'
+comment on column IF_MPRO_DET.ATWRT_002_02 is '개폐기 스위칭매체 (개폐기이면 필수 입력) 1-400 2-600 3-630'
 /
 
-comment on column XMLEDI_MPRO_DET.ATWRT_002_03 is '개폐기 절연매체 (개폐기이면 필수 입력) 1-SF6 2-Vacuum 3-Oil 4-기타'
+comment on column IF_MPRO_DET.ATWRT_002_03 is '개폐기 절연매체 (개폐기이면 필수 입력) 1-SF6 2-Vacuum 3-Oil 4-기타'
 /
 
-comment on column XMLEDI_MPRO_DET.ATWRT_002_04 is '개폐기 매커니즘 조작방식 (개폐기이면 필수 입력) 1-Spring 2-Magnetic 3-기타'
+comment on column IF_MPRO_DET.ATWRT_002_04 is '개폐기 매커니즘 조작방식 (개폐기이면 필수 입력) 1-Spring 2-Magnetic 3-기타'
 /
 
-comment on column XMLEDI_MPRO_DET.ATWRT_002_05 is '개폐기 1차 부싱타입 (개폐기이면 필수 입력) 1-자기제 2-폴리머 3-에폭시'
+comment on column IF_MPRO_DET.ATWRT_002_05 is '개폐기 1차 부싱타입 (개폐기이면 필수 입력) 1-자기제 2-폴리머 3-에폭시'
 /
 
-comment on column XMLEDI_MPRO_DET.ATWRT_002_06 is '개폐기 2차 부싱타입 (개폐기이면 필수 입력) 1-자기제 2-폴리머 3-에폭시'
+comment on column IF_MPRO_DET.ATWRT_002_06 is '개폐기 2차 부싱타입 (개폐기이면 필수 입력) 1-자기제 2-폴리머 3-에폭시'
 /
 
-comment on column XMLEDI_MPRO_DET.ATWRT_002_07 is '개폐기 정격가스압력(Mpa) (개폐기이면 필수 입력) 소숫점 2자리까지'
+comment on column IF_MPRO_DET.ATWRT_002_07 is '개폐기 정격가스압력(Mpa) (개폐기이면 필수 입력) 소숫점 2자리까지'
 /
 
-comment on column XMLEDI_MPRO_DET.ATWRT_002_08 is '개폐기 최저보증가스압력(Mpa) (개폐기이면 필수 입력) 소숫점 2자리까지'
+comment on column IF_MPRO_DET.ATWRT_002_08 is '개폐기 최저보증가스압력(Mpa) (개폐기이면 필수 입력) 소숫점 2자리까지'
 /
 
-comment on column XMLEDI_MPRO_DET.ATWRT_002_09 is '개폐기 가스량(Kg) (개폐기이면 필수 입력) 소숫점 2자리까지'
+comment on column IF_MPRO_DET.ATWRT_002_09 is '개폐기 가스량(Kg) (개폐기이면 필수 입력) 소숫점 2자리까지'
 /
 
-comment on column XMLEDI_MPRO_DET.RECVST is '개별자재 생산내역서 내용 점검 결과(S : 성공, E : 실패) (ERP 자동기록)※ ERP 스마트 물류관리 시스템이 수신 시  내용 Check 후 행별로 결과 기록
+comment on column IF_MPRO_DET.RECVST is '개별자재 생산내역서 내용 점검 결과(S : 성공, E : 실패) (ERP 자동기록)※ ERP 스마트 물류관리 시스템이 수신 시  내용 Check 후 행별로 결과 기록
 '
 /
 
-comment on column XMLEDI_MPRO_DET.RECVMG is '개별자재 생산내역서 내용 점검 결과(ERP 자동기록)※ ERP 스마트 물류관리 시스템이 수신 시 내용 Check 후 오류 시 메세지 기록
+comment on column IF_MPRO_DET.RECVMG is '개별자재 생산내역서 내용 점검 결과(ERP 자동기록)※ ERP 스마트 물류관리 시스템이 수신 시 내용 Check 후 오류 시 메세지 기록
 '
 /
--- db/EBXMLWEB_XMLEDI_MPRO_MST.sql
+++ db/EBXMLWEB_XMLEDI_MPRO_MST.sql
@@ -1,5 +1,5 @@
 -- auto-generated definition
-create table XMLEDI_MPRO_MST
+create table IF_MPRO_MST
 (
     EBELN     VARCHAR2(10) not null,
     LIFNR     VARCHAR2(10) not null,
@@ -31,99 +31,99 @@ create table XMLEDI_MPRO_MST
     RECVST    VARCHAR2(1),
     RECVMG    VARCHAR2(250),
     INPUT_ST  VARCHAR2(1),
-    constraint XMLEDI_MPRO_MST_PK
+    constraint IF_MPRO_MST_PK
         primary key (EBELN, LIFNR, LIFNR_GR, EBELP)
 )
 /
 
-comment on column XMLEDI_MPRO_MST.EBELN is '주문번호 (ERP 자동기록)'
+comment on column IF_MPRO_MST.EBELN is '주문번호 (ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_MST.LIFNR is '계약업체 (ERP 자동기록)'
+comment on column IF_MPRO_MST.LIFNR is '계약업체 (ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_MST.LIFNR_GR is '납품업체 (ERP 자동기록)'
+comment on column IF_MPRO_MST.LIFNR_GR is '납품업체 (ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_MST.EBELP is '품목번호 (ERP 자동기록)'
+comment on column IF_MPRO_MST.EBELP is '품목번호 (ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_MST.BEDAT is '주문일자 (ERP 자동기록)'
+comment on column IF_MPRO_MST.BEDAT is '주문일자 (ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_MST.EINDT is '납기일자 (ERP 자동기록)'
+comment on column IF_MPRO_MST.EINDT is '납기일자 (ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_MST.LIFNR_NM is '계약업체명 (ERP 자동기록)'
+comment on column IF_MPRO_MST.LIFNR_NM is '계약업체명 (ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_MST.LIFNR_GRNM is '납품업체명 (ERP 자동기록)'
+comment on column IF_MPRO_MST.LIFNR_GRNM is '납품업체명 (ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_MST.WAERS is '통화 (ERP 자동기록)'
+comment on column IF_MPRO_MST.WAERS is '통화 (ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_MST.NAME1 is '입고사업소명 (ERP 자동기록)'
+comment on column IF_MPRO_MST.NAME1 is '입고사업소명 (ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_MST.MATNR is '자재번호 (ERP 자동기록)'
+comment on column IF_MPRO_MST.MATNR is '자재번호 (ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_MST.TXZ01 is '자재명 (ERP 자동기록)'
+comment on column IF_MPRO_MST.TXZ01 is '자재명 (ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_MST.MENGE is '주문수량 (ERP 자동기록)'
+comment on column IF_MPRO_MST.MENGE is '주문수량 (ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_MST.NETPR is '단가 (ERP 자동기록)'
+comment on column IF_MPRO_MST.NETPR is '단가 (ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_MST.NETWR is '금액 (ERP 자동기록)'
+comment on column IF_MPRO_MST.NETWR is '금액 (ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_MST.ZBPMNG is '납품수량 (ERP 자동기록)'
+comment on column IF_MPRO_MST.ZBPMNG is '납품수량 (ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_MST.MEINS is '단위 (ERP 자동기록)'
+comment on column IF_MPRO_MST.MEINS is '단위 (ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_MST.WAERS is '통화 (ERP 자동기록)'
+comment on column IF_MPRO_MST.WAERS is '통화 (ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_MST.PRUEFLOS is '검사LOT번호 (ERP 자동기록)'
+comment on column IF_MPRO_MST.PRUEFLOS is '검사LOT번호 (ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_MST.MATSN_FR is '고유인식번호 시작 (ERP 자동기록)'
+comment on column IF_MPRO_MST.MATSN_FR is '고유인식번호 시작 (ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_MST.MATSN_TO is '고유인식번호 종료 (ERP 자동기록)'
+comment on column IF_MPRO_MST.MATSN_TO is '고유인식번호 종료 (ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_MST.MATSN_CNT is '고유인식번호 발급 개수 (ERP 자동기록)'
+comment on column IF_MPRO_MST.MATSN_CNT is '고유인식번호 발급 개수 (ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_MST.ATWRT_TP is '자재 특성유형 (ERP 자동기록)(001 : 변압기, 002 : 개폐기)'
+comment on column IF_MPRO_MST.ATWRT_TP is '자재 특성유형 (ERP 자동기록)(001 : 변압기, 002 : 개폐기)'
 /
 
-comment on column XMLEDI_MPRO_MST.SENDDT is '개별자재 생산내역서 송신일자(ERP to EDI)(ERP 자동기록)'
+comment on column IF_MPRO_MST.SENDDT is '개별자재 생산내역서 송신일자(ERP to EDI)(ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_MST.SENDTM is '개별자재 생산내역서 송신시간(ERP to EDI)(ERP 자동기록)'
+comment on column IF_MPRO_MST.SENDTM is '개별자재 생산내역서 송신시간(ERP to EDI)(ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_MST.RECVDT is '개별자재 생산내역서 수신일자(EDI to ERP)(ERP 자동기록)'
+comment on column IF_MPRO_MST.RECVDT is '개별자재 생산내역서 수신일자(EDI to ERP)(ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_MST.RECVTM is '개별자재 생산내역서 수신시간(EDI to ERP)(ERP 자동기록)'
+comment on column IF_MPRO_MST.RECVTM is '개별자재 생산내역서 수신시간(EDI to ERP)(ERP 자동기록)'
 /
 
-comment on column XMLEDI_MPRO_MST.RECVST is '개별자재 생산내역서 수신결과(S : 성공, E : 실패) (ERP 자동기록)※ ERP 스마트 물류관리 시스템이 수신 시  내용 Check 후 결과 기록'
+comment on column IF_MPRO_MST.RECVST is '개별자재 생산내역서 수신결과(S : 성공, E : 실패) (ERP 자동기록)※ ERP 스마트 물류관리 시스템이 수신 시  내용 Check 후 결과 기록'
 /
 
-comment on column XMLEDI_MPRO_MST.RECVMG is '개별자재 생산내역서 수신오류 메세지(ERP 자동기록)※ ERP 스마트 물류관리 시스템이 수신 시 오류 메세지 기록'
+comment on column IF_MPRO_MST.RECVMG is '개별자재 생산내역서 수신오류 메세지(ERP 자동기록)※ ERP 스마트 물류관리 시스템이 수신 시 오류 메세지 기록'
 /
 
-comment on column XMLEDI_MPRO_MST.INPUT_ST is 'EDI 입력 상태정보(I-초기Default(ERP 기록), S-Save 저장, C-확정)'
+comment on column IF_MPRO_MST.INPUT_ST is 'EDI 입력 상태정보(I-초기Default(ERP 기록), S-Save 저장, C-확정)'
 /
 
--- INSERT INTO EBXMLWEB.XMLEDI_MPRO_MST (EBELN, LIFNR, LIFNR_GR, EBELP, MATNR, TXZ01, MENGE, NETPR, NETWR, ZBPMNG, MEINS, WAERS, MATSN_FR, MATSN_TO, MATSN_CNT, ATWRT_TP, SENDDT, SENDTM, RECVDT, RECVTM, RECVST, RECVMG, INPUT_ST) VALUES ('4100100208', '1188200345', '1188200345', '12345', '126474', '고효율주상변압기,100KVA', '10', '1931510', '7726080', '10', '111', '55555', '2021020100000001', '2021020100000010', 10, '001', '20220416', '101010', '20220416', '151515', 'S', '메세지', 'S');
+-- INSERT INTO EBXMLWEB.IF_MPRO_MST (EBELN, LIFNR, LIFNR_GR, EBELP, MATNR, TXZ01, MENGE, NETPR, NETWR, ZBPMNG, MEINS, WAERS, MATSN_FR, MATSN_TO, MATSN_CNT, ATWRT_TP, SENDDT, SENDTM, RECVDT, RECVTM, RECVST, RECVMG, INPUT_ST) VALUES ('4100100208', '1188200345', '1188200345', '12345', '126474', '고효율주상변압기,100KVA', '10', '1931510', '7726080', '10', '111', '55555', '2021020100000001', '2021020100000010', 10, '001', '20220416', '101010', '20220416', '151515', 'S', '메세지', 'S');
